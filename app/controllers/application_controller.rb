09b3e7e09773944013df879720a013b171fd29e2
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-07-27 14:46:46 +0300

    update app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  helper_method :collection, :resource, :current_user
+  attr_reader :current_user, :current_auth_token_value
+
+  helper_method :collection, :resource
 
   before_action :authenticate!
-
-  rescue_from AuthorizationError do
-    head :unauthorized
-  end
 
   rescue_from ActionController::ParameterMissing do |exception|
     @exception = exception
 
     render :exception, status: :unprocessable_entity
-  end
-
-  rescue_from ActiveRecord::RecordInvalid, ActiveModel::StrictValidationFailed do
-    render :errors, status: :unprocessable_entity
   end
 
   rescue_from ActiveRecord::RecordNotFound do
@@ -27,18 +21,33 @@ class ApplicationController < ActionController::Base
     render :exception, status: :not_found
   end
 
+  rescue_from ActiveRecord::RecordInvalid, ActiveModel::StrictValidationFailed do
+    render :errors, status: :unprocessable_entity
+  end
+
   def create
     render :errors unless resource.save
   end
 
-  def current_user
+  private
+
+  def authenticate!
     authenticate_or_request_with_http_token do |token, options|
-      @current_user = User.joins(:auth_token).find_by(auth_tokens: { value: token })
+      @current_auth_token_value = token
+
+      @current_user = User.joins(:auth_token).find_by auth_tokens: { value: token }
     end
   end
 
-  private
-  def authenticate!
-    current_user || raise(AuthorizationError)
+  def authenticate
+    authenticate_with_http_token do |token, options|
+      @current_auth_token_value = token
+
+      @current_user = User.joins(:auth_token).find_by auth_tokens: { value: token }
+    end
+  end
+
+  def current_auth_token
+    @current_auth_token ||= AuthToken.find_by_value current_auth_token_value
   end
 end

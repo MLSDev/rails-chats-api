402da9c88c5986663771eab3e5feb80a5ca0621b
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-07-27 13:54:16 +0300

    update spec/factories/users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Internet.user_name }
-    email { Faker::Internet.email }
+    name     { Faker::Internet.user_name } if User.new.attributes.symbolize_keys.include?(:name)
+
+    email    { Faker::Internet.email }
+
     password { Faker::Internet.password }
+
+    trait :with_auth_token do
+      association :auth_token
+    end
   end
 end

20cfe1cf0cb17e6908c941db4282b38e19b36f4b
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-07-27 14:46:35 +0300

    update spec/requests/sign_out_spec.rb

--- spec/requests/sign_out_spec.rb
+++ spec/requests/sign_out_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'SignOut', type: :request do
-  let(:user) { create(:user)}
-
-  let(:token) { AuthToken.create(user_id: user.id) }
-
-  let(:value) { token.value }
-
-  let(:headers) { { 'Authorization' => "Token token=#{value}", 'Content-type' => 'application/json', 'Accept' => 'application/json' } }
-
-  before { delete '/session', params: {} , headers: headers }
+  let!(:user) { create :user, :with_auth_token }
 
   context do
-    it("destroys current_user's auth_token") { expect(user.auth_token).to eq nil }
+    before { delete '/session', params: {} , headers: authorized_headers(user.auth_token.value) }
 
-    it('returns HTTP Status Code 204') { expect(response).to have_http_status 204 }
+    before { user.reload }
+
+    it("destroys current_user's auth_token") { expect(user.auth_token).to be_nil }
+
+    it('returns HTTP Status Code 204') { expect(response).to have_http_status :no_content }
+  end
+
+  context 'Unauthorized' do
+    before { delete '/session', params: {} , headers: authorized_headers(FFaker::Code.ean) }
+
+    before { user.reload }
+
+    it('returns HTTP Status Code 204') { expect(response).to have_http_status :unauthorized }
   end
 end

90682bb98699ad9ae82ca5815efce7488534d080
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-07-27 14:27:56 +0300

    add represent_boolean_as_integer option for test env

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,10 @@ module ChatApplication
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #
+    # for sqlite usage
+    #
+    config.active_record.sqlite3.represent_boolean_as_integer = true if Rails.env.test?
   end
 end

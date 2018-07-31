7fd1ab214f1357aa48822886ac394c4c7c0130e3
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-07-31 12:23:53 +0300

    cleanup app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  attr_reader :current_user, :current_auth_token_value
+  attr_reader :current_user
 
   helper_method :collection, :resource
 
